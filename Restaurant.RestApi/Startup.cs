caafdf15c63a9da11b55ad06b8c2a418a2365e7e
Mark Seemann <mark@ploeh.dk>
2020-02-25 15:30:55 +0100

    Add FxCop analyzers
    This emits 7 warnings on auto-generated code, so this commit also
    addresses those warnings.

--- Restaurant.RestApi/Startup.cs
+++ Restaurant.RestApi/Startup.cs
@@ -11,19 +11,13 @@ using Microsoft.Extensions.Hosting;
 
 namespace Ploeh.Samples.Restaurant.RestApi
 {
-    public class Startup
+    public sealed class Startup
     {
-        // This method gets called by the runtime. Use this method to add
-        // services to the container.
-        // For more information on how to configure your application,
-        // visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
-
         // This method gets called by the runtime. Use this method to configure
         // the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public static void Configure(
+            IApplicationBuilder app,
+            IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -36,7 +30,8 @@ namespace Ploeh.Samples.Restaurant.RestApi
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello World!")
+                        .ConfigureAwait(false);
                 });
             });
         }
