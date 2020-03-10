3ee0733c7e7c1c2158829c6f96dff25428859096
Mark Seemann <mark@ploeh.dk>
2020-03-10 09:22:17 +0100

    Add is-ok test for home resource
    A couple of notes:
    
    - The test isn't too restrictive in its assertion. Ultimately, behaviour
      ought to change. The only behaviour I'm confident should remain is
      that it should return some success status code. That's the only
      property that the assertion verifies. It doesn't look at the content
      or headers yet.
    - I updated the build script to run tests.
    - The test follows my AAA formatting heuristic.
    - Notice the error message supplied as an option argument to
      Assert.True.
    - The test is too verbose, because of the FxCop rules.

---
+++ Restaurant.RestApi.Tests/HomeTests.cs
@@ -0,0 +1,28 @@
+﻿/* Copyright (c) Mark Seemann 2020. All rights reserved. */
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Ploeh.Samples.Restaurant.RestApi.Tests
+{
+    public class HomeTests
+    {
+        [Fact]
+        public async Task HomeIsOk()
+        {
+            using var factory = new WebApplicationFactory<Startup>();
+            var client = factory.CreateClient();
+
+            var response = await client
+                .GetAsync(new Uri("", UriKind.Relative))
+                .ConfigureAwait(false);
+
+            Assert.True(
+                response.IsSuccessStatusCode,
+                $"Actual status code: {response.StatusCode}.");
+        }
+    }
+}
