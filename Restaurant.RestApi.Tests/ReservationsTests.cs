90e48692d362b2bc7338eb3a5f8c0277029327fc
Mark Seemann <mark@ploeh.dk>
2020-03-10 21:03:50 +0100

    Add reservations resource
    This is another outside-in test that provokes the creation of a new
    controller class. Its implementation is as simple as I could make it.
    
    About the test:
    - I created a PostReservation helper method, because I think it's
      irrelevant exactly how the client makes the HTTP request. It isn't
      quite, but if time and space allows, I'd like to define a client that
      follows links Richardson-maturity-model style. A helper API should
      ease any such transition.
    - For now, I'm not supplying an ID for the reservation. This is a
      delibarate design flaw that I'm introducing now in order to show how
      to refactor later.

---
+++ Restaurant.RestApi.Tests/ReservationsTests.cs
@@ -0,0 +1,46 @@
+﻿/* Copyright (c) Mark Seemann 2020. All rights reserved. */
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Ploeh.Samples.Restaurant.RestApi.Tests
+{
+    public class ReservationsTests
+    {
+        [Fact]
+        public async Task PostValidReservation()
+        {
+            var response = await PostReservation(new {
+                date = "2023-03-10 19:00",
+                email = "katinka@example.com",
+                name = "Katinka Ingabogovinanana",
+                quantity = 2 });
+
+            Assert.True(
+                response.IsSuccessStatusCode,
+                $"Actual status code: {response.StatusCode}.");
+        }
+
+        [SuppressMessage(
+            "Usage",
+            "CA2234:Pass system uri objects instead of strings",
+            Justification = "URL isn't passed as variable, but as literal.")]
+        private async Task<HttpResponseMessage> PostReservation(
+            object reservation)
+        {
+            using var factory = new WebApplicationFactory<Startup>();
+            var client = factory.CreateClient();
+
+            string json = JsonSerializer.Serialize(reservation);
+            using var content = new StringContent(json);
+            content.Headers.ContentType.MediaType = "application/json";
+            return await client.PostAsync("reservations", content);
+        }
+    }
+}
---
+++ Restaurant.RestApi/ReservationsController.cs
@@ -0,0 +1,17 @@
+﻿/* Copyright (c) Mark Seemann 2020. All rights reserved. */
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ploeh.Samples.Restaurant.RestApi
+{
+    [Route("[controller]")]
+    public class ReservationsController
+    {
+#pragma warning disable CA1822 // Mark members as static
+        public void Post() { }
+#pragma warning restore CA1822 // Mark members as static
+    }
+}
