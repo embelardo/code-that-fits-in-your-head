c5b405ea1d90c48acb985a91b00c5d495e029dc8
Mark Seemann <mark@ploeh.dk>
2020-04-30 13:22:41 +0200

    Accept reservation if allocation is possible
    This is beginning to look more appropriate for a configuration with pure
    communal tables.
    
    The code is beginning to look complex. The cyclomatic complexity is 5,
    so there's still a bit of head room there, but the line count is 23,
    which is uncomfortably close to the 24 line maximum.
    
    I'm also not convinced that null inputs should cause exceptions. If I
    were to follow Postel's law, I should return false on null reservations.
    That would require a deliberate design decision, though, so I think that
    there ought to be tests for those scenarios.

--- Restaurant.RestApi.Tests/MaitreDTests.cs
+++ Restaurant.RestApi.Tests/MaitreDTests.cs
@@ -10,20 +10,26 @@ namespace Ploeh.Samples.Restaurant.RestApi.Tests
     public class MaitreDTests
     {
         [Theory]
-        [InlineData(new[] { 12 })]
-        [InlineData(new[] { 8, 11 })]
-        public void Accept(int[] tableSeats)
+        [InlineData(new[]    { 12 },  new int[0])]
+        [InlineData(new[] { 8, 11 },  new int[0])]
+        [InlineData(new[] { 2, 11 }, new[] { 2 })]
+        public void Accept(int[] tableSeats, int[] reservedSeats)
         {
             var tables =
                 tableSeats.Select(s => new Table(TableType.Communal, s));
             var sut = new MaitreD(tables);
 
+            var rs = reservedSeats.Select(s => new Reservation(
+                new DateTime(2022, 4, 1, 20, 15, 0),
+                "x@example.net",
+                "",
+                s));
             var r = new Reservation(
                 new DateTime(2022, 4, 1, 20, 15, 0),
                 "x@example.net",
                 "",
                 11);
-            var actual = sut.WillAccept(Array.Empty<Reservation>(), r);
+            var actual = sut.WillAccept(rs, r);
 
             Assert.True(actual);
         }
--- Restaurant.RestApi/MaitreD.cs
+++ Restaurant.RestApi/MaitreD.cs
@@ -8,26 +8,39 @@ namespace Ploeh.Samples.Restaurant.RestApi
 {
     public sealed class MaitreD
     {
-        private readonly int largestTableSize;
-
-        public MaitreD(params Table[] tables)
-        {
-            largestTableSize = tables.Max(t => t.Seats);
-        }
-
-        public MaitreD(IEnumerable<Table> tables) : this(tables.ToArray())
+        public MaitreD(params Table[] tables) : this(tables.AsEnumerable())
         {
         }
+
+        public MaitreD(IEnumerable<Table> tables)
+        {
+            Tables = tables;
+        }
+
+        public IEnumerable<Table> Tables { get; }
 
         public bool WillAccept(
             IEnumerable<Reservation> existingReservations,
             Reservation candidate)
         {
+            if (existingReservations is null)
+                throw new ArgumentNullException(nameof(existingReservations));
             if (candidate is null)
                 throw new ArgumentNullException(nameof(candidate));
 
-            int reservedSeats = existingReservations.Sum(r => r.Quantity);
-            return reservedSeats + candidate.Quantity <= largestTableSize;
+            List<Table> availableTables = Tables.ToList();
+            foreach (var r in existingReservations)
+            {
+                var table = availableTables.Find(t => r.Quantity <= t.Seats);
+                if (table is { })
+                {
+                    availableTables.Remove(table);
+                    availableTables.Add(
+                        new Table(table.TableType, table.Seats - r.Quantity));
+                }
+            }
+
+            return availableTables.Any(t => candidate.Quantity <= t.Seats);
         }
     }
 }
