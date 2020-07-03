5871ba57bec878da595a1df716cf9c48780a5340
Mark Seemann <mark@ploeh.dk>
2020-07-03 09:18:23 +0200

    Check month on calendar representation

--- Restaurant.RestApi.Tests/CalendarTests.cs
+++ Restaurant.RestApi.Tests/CalendarTests.cs
@@ -91,15 +91,21 @@ namespace Ploeh.Samples.Restaurant.RestApi.Tests
             "Performance",
             "CA1812: Avoid uninstantiated internal classes",
             Justification = "This class is instantiated via Reflection.")]
-        private class CalendarTestCases : TheoryData<Func<CalendarController, ActionResult>, int, int, int>
+        private class CalendarTestCases :
+            TheoryData<Func<CalendarController, ActionResult>, int, int?, int, int>
         {
             public CalendarTestCases()
             {
-                Add(sut => sut.Get(2000), 2000, 366, 10);
-                Add(sut => sut.Get(2019), 2019, 365, 20);
-                Add(sut => sut.Get(2020), 2020, 366,  5);
-                Add(sut => sut.Get(2040), 2040, 366, 10);
-                Add(sut => sut.Get(2100), 2100, 365,  8);
+                AddYear(2000, 366, 10);
+                AddYear(2019, 365, 20);
+                AddYear(2020, 366,  5);
+                AddYear(2040, 366, 10);
+                AddYear(2100, 365,  8);
+            }
+
+            private void AddYear(int year, int expectedDays, int tableSize)
+            {
+                Add(sut => sut.Get(year), year, null, expectedDays, tableSize);
             }
         }
 
@@ -111,6 +117,7 @@ namespace Ploeh.Samples.Restaurant.RestApi.Tests
         public void GetYear(
             Func<CalendarController, ActionResult> act,
             int year,
+            int? month,
             int expectedDays,
             int tableSize)
         {
@@ -121,6 +128,7 @@ namespace Ploeh.Samples.Restaurant.RestApi.Tests
             var ok = Assert.IsAssignableFrom<OkObjectResult>(actual);
             var dto = Assert.IsAssignableFrom<CalendarDto>(ok.Value);
             Assert.Equal(year, dto.Year);
+            Assert.Equal(month, dto.Month);
             Assert.NotNull(dto.Days);
             var days = dto.Days;
             Assert.Equal(expectedDays, days?.Length);
