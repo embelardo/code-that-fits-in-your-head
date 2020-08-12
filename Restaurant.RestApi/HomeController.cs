679e917a0a564b7cf110af6438449306c6543bb0
Mark Seemann <mark@ploeh.dk>
2020-08-12 14:53:57 +0200

    Move home link generation to filter

--- Restaurant.RestApi/HomeController.cs
+++ Restaurant.RestApi/HomeController.cs
@@ -2,36 +2,22 @@
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Ploeh.Samples.Restaurant.RestApi
 {
     [Route("")]
-    public class HomeController : ControllerBase
+    public sealed class HomeController
     {
-        private readonly bool enableCalendar;
-
-        public HomeController(CalendarFlag calendarFlag)
+        [SuppressMessage(
+            "Performance",
+            "CA1822:Mark members as static",
+            Justification = "Controller methods must be instance methods.")]
+        public ActionResult Get()
         {
-            if (calendarFlag is null)
-                throw new ArgumentNullException(nameof(calendarFlag));
-
-            enableCalendar = calendarFlag.Enabled;
-        }
-
-        public IActionResult Get()
-        {
-            var links = new List<LinkDto>();
-            links.Add(Url.LinkToReservations());
-            if (enableCalendar)
-            {
-                var now = DateTime.Now;
-                links.Add(Url.LinkToYear(now.Year));
-                links.Add(Url.LinkToMonth(now.Year, now.Month));
-                links.Add(Url.LinkToDay(now.Year, now.Month, now.Day));
-            }
-            return Ok(new HomeDto { Links = links.ToArray() });
+            return new OkObjectResult(new HomeDto());
         }
     }
 }
--- Restaurant.RestApi/LinksFilter.cs
+++ Restaurant.RestApi/LinksFilter.cs
@@ -16,11 +16,16 @@ namespace Ploeh.Samples.Restaurant.RestApi
         Justification = "This class is instantiated via Reflection.")]
     internal class LinksFilter : IAsyncActionFilter
     {
+        private readonly bool enableCalendar;
+
         public IUrlHelperFactory UrlHelperFactory { get; }
 
-        public LinksFilter(IUrlHelperFactory urlHelperFactory)
+        public LinksFilter(
+            IUrlHelperFactory urlHelperFactory,
+            CalendarFlag calendarFlag)
         {
             UrlHelperFactory = urlHelperFactory;
+            enableCalendar = calendarFlag.Enabled;
         }
 
         public async Task OnActionExecutionAsync(
@@ -34,11 +39,33 @@ namespace Ploeh.Samples.Restaurant.RestApi
             var url = UrlHelperFactory.GetUrlHelper(ctxAfter);
             switch (ok.Value)
             {
+                case HomeDto homeDto:
+                    AddLinks(homeDto, url);
+                    break;
                 case CalendarDto calendarDto:
                     AddLinks(calendarDto, url);
                     break;
                 default:
                     break;
+            }
+        }
+
+        private void AddLinks(HomeDto dto, IUrlHelper url)
+        {
+            if (enableCalendar)
+            {
+                var now = DateTime.Now;
+                dto.Links = new[]
+                {
+                    url.LinkToReservations(),
+                    url.LinkToYear(now.Year),
+                    url.LinkToMonth(now.Year, now.Month),
+                    url.LinkToDay(now.Year, now.Month, now.Day)
+                };
+            }
+            else
+            {
+                dto.Links = new[] { url.LinkToReservations() };
             }
         }
 
