ea2e873a106ed743f8ceb8e728378fc9261ddbae
Mark Seemann <mark@ploeh.dk>
2020-08-22 11:56:01 +0200

    Extract FsCheck generators to separate file
    I would have preferred to name the static class `Gen`, like I would do
    in F#, but this isn't feasible in C#, as far as I can tell. I settled
    for `Gens` instead, to indicate that this is a class that holds plural
    `Gen` values.

--- Restaurant.RestApi.Tests/ScheduleTests.cs
+++ Restaurant.RestApi.Tests/ScheduleTests.cs
@@ -16,8 +16,8 @@ namespace Ploeh.Samples.Restaurant.RestApi.Tests
         public Property Schedule()
         {
             return Prop.ForAll(
-                GenReservations
-                    .SelectMany(rs => GenMaitreD(rs).Select(m => (m, rs)))
+                Gens.Reservations
+                    .SelectMany(rs => Gens.MaitreD(rs).Select(m => (m, rs)))
                     .ToArbitrary(),
                 t => ScheduleImp(t.m, t.rs));
         }
@@ -88,104 +88,6 @@ namespace Ploeh.Samples.Restaurant.RestApi.Tests
                 if (reservation is { })
                     yield return reservation;
             }
-        }
-
-        private static Gen<Email> GenEmail =>
-            from s in Arb.Default.NonWhiteSpaceString().Generator
-            select new Email(s.Item);
-
-        private static Gen<Name> GenName =>
-            from s in Arb.Default.StringWithoutNullChars().Generator
-            select new Name(s.Item);
-
-        private static Gen<Reservation> GenReservation =>
-            from id in Arb.Default.Guid().Generator
-            from d in Arb.Default.DateTime().Generator
-            from e in GenEmail
-            from n in GenName
-            from q in Arb.Default.PositiveInt().Generator
-            select new Reservation(id, d, e, n, q.Item);
-
-        private static Gen<Reservation[]> GenReservations
-        {
-            get
-            {
-                var normalArrayGen = GenReservation.ArrayOf();
-                var adjacentReservationsGen = GenReservation.ArrayOf()
-                    .SelectMany(rs => Gen
-                        .Sequence(rs.Select(GenAdjacentReservations))
-                        .SelectMany(rss => Gen.Shuffle(
-                            rss.SelectMany(rs => rs))));
-                return Gen.OneOf(normalArrayGen, adjacentReservationsGen);
-            }
-        }
-
-        /// <summary>
-        /// Generate an adjacant reservation with a 25% chance.
-        /// </summary>
-        /// <param name="reservation">The candidate reservation</param>
-        /// <returns>
-        /// A generator of an array of reservations. The generated array is
-        /// either a singleton or a pair. In 75% of the cases, the input
-        /// <paramref name="reservation" /> is returned as a singleton array.
-        /// In 25% of the cases, the array contains two reservations: the input
-        /// reservation as well as another reservation adjacent to it.
-        /// </returns>
-        private static Gen<Reservation[]> GenAdjacentReservations(
-            Reservation reservation)
-        {
-            return
-                from adjacent in GenReservationAdjacentTo(reservation)
-                from useAdjacent in Gen.Frequency(
-                    new WeightAndValue<Gen<bool>>(3, Gen.Constant(false)),
-                    new WeightAndValue<Gen<bool>>(1, Gen.Constant(true)))
-                let rs = useAdjacent ?
-                    new[] { reservation, adjacent } :
-                    new[] { reservation }
-                select rs;
-        }
-
-        private static Gen<Reservation> GenReservationAdjacentTo(
-            Reservation reservation)
-        {
-            return
-                from minutes in Gen.Choose(-6 * 4, 6 * 4) // 4: quarters/h
-                from r in GenReservation
-                select r.WithDate(
-                    reservation.At + TimeSpan.FromMinutes(minutes));
-        }
-
-        private static Gen<MaitreD> GenMaitreD(
-            IEnumerable<Reservation> reservations)
-        {
-            return
-                from seatingDuration in Gen.Choose(1, 6)
-                from tables in GenTables(reservations)
-                select new MaitreD(
-                    TimeSpan.FromHours(18),
-                    TimeSpan.FromHours(21),
-                    TimeSpan.FromHours(seatingDuration),
-                    tables);
-        }
-
-        /// <summary>
-        /// Generate a table configuration that can at minimum accomodate all
-        /// reservations.
-        /// </summary>
-        /// <param name="reservations">The reservations to accommodate</param>
-        /// <returns>A generator of valid table configurations.</returns>
-        private static Gen<IEnumerable<Table>> GenTables(
-            IEnumerable<Reservation> reservations)
-        {
-            // Create a table for each reservation, to ensure that all
-            // reservations can be allotted a table.
-            var tables = reservations.Select(r => Table.Standard(r.Quantity));
-            return
-                from moreTables in
-                    Gen.Choose(1, 12).Select(Table.Standard).ArrayOf()
-                let allTables =
-                    tables.Concat(moreTables).OrderBy(t => t.Capacity)
-                select allTables.AsEnumerable();
         }
     }
 }
