f1476391cc14434dc3641536218592f727014f0c
Mark Seemann <mark@ploeh.dk>
2020-09-10 14:20:29 +0200

    Add LoggingReservationsRepository
    While this commit is a start of a logging Decorator for the
    IReservationsRepository interface, it's mostly a partial commit to
    experiment with logging on Azure.

---
+++ Restaurant.RestApi/LoggingReservationsRepository.cs
@@ -0,0 +1,57 @@
+﻿/* Copyright (c) Mark Seemann 2020. All rights reserved. */
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Ploeh.Samples.Restaurant.RestApi
+{
+    public sealed class LoggingReservationsRepository : IReservationsRepository
+    {
+        public LoggingReservationsRepository(
+            ILogger<LoggingReservationsRepository> logger,
+            IReservationsRepository inner)
+        {
+            Logger = logger;
+            Inner = inner;
+        }
+
+        public ILogger<LoggingReservationsRepository> Logger { get; }
+        public IReservationsRepository Inner { get; }
+
+        public async Task Create(int restaurantId, Reservation reservation)
+        {
+            Logger.LogInformation(
+                "{method}(restaurantId: {restaurantId}, reservation: {reservation})",
+                nameof(Create),
+                restaurantId,
+                JsonSerializer.Serialize(reservation.ToDto()));
+            await Inner.Create(restaurantId, reservation).ConfigureAwait(false);
+        }
+
+        public Task Delete(Guid id)
+        {
+            return Inner.Delete(id);
+        }
+
+        public Task<Reservation?> ReadReservation(Guid id)
+        {
+            return Inner.ReadReservation(id);
+        }
+
+        public Task<IReadOnlyCollection<Reservation>> ReadReservations(
+            int restaurantId,
+            DateTime min,
+            DateTime max)
+        {
+            return Inner.ReadReservations(restaurantId, min, max);
+        }
+
+        public Task Update(Reservation reservation)
+        {
+            return Inner.Update(reservation);
+        }
+    }
+}
--- Restaurant.RestApi/Startup.cs
+++ Restaurant.RestApi/Startup.cs
@@ -14,6 +14,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
 using Ploeh.Samples.Restaurant.RestApi.Options;
 
@@ -49,8 +50,14 @@ namespace Ploeh.Samples.Restaurant.RestApi
             ConfigureAuthorization(services);
 
             var connStr = Configuration.GetConnectionString("Restaurant");
-            services.AddSingleton<IReservationsRepository>(
-                new SqlReservationsRepository(connStr));
+            services.AddSingleton<IReservationsRepository>(sp =>
+            {
+                var logger =
+                    sp.GetService<ILogger<LoggingReservationsRepository>>();
+                return new LoggingReservationsRepository(
+                    logger,
+                    new SqlReservationsRepository(connStr));
+            });
 
             var restaurantsOptions = Configuration.GetSection("Restaurants")
                 .Get<RestaurantOptions[]>();
