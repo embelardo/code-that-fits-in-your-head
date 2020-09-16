aa692596a3b08cd016dfc09e3fa171ccfcb2f1a2
Mark Seemann <mark@ploeh.dk>
2020-09-16 07:18:01 +0200

    Delete Either API
    It turns out that I didn't need it after all...

--- Restaurant.RestApi/IEitherVisitor.cs
+++
@@ -1,9 +0,0 @@
-﻿/* Copyright (c) Mark Seemann 2020. All rights reserved. */
-namespace Ploeh.Samples.Restaurants.RestApi
-{
-    public interface IEitherVisitor<TL, TR, T>
-    {
-        T VisitLeft(TL l);
-        T VisitRight(TR r);
-    }
-}
