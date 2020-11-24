2b17f7505bf5e085b75fca1abe075a279fad3651
Mark Seemann <mark@ploeh.dk>
2020-11-24 18:26:02 +0100

    Remove redundant code analysis suppression
    I'm not sure, and I haven't investigated, but Visual Studio came out and
    told me in a friendly way that this suppression wasn't necessary. I
    wonder if this is because of an updated analysis rule.

--- Restaurant.RestApi.Tests/HomeTests.cs
+++ Restaurant.RestApi.Tests/HomeTests.cs
@@ -1,12 +1,8 @@
 ﻿/* Copyright (c) Mark Seemann 2020. All rights reserved. */
-using Microsoft.AspNetCore.Mvc.Testing;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Net.Http;
-using System.Text;
-using System.Text.Json;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -15,9 +11,6 @@ namespace Ploeh.Samples.Restaurants.RestApi.Tests
     public class HomeTests
     {
         [Fact]
-        [SuppressMessage(
-            "Usage", "CA2234:Pass system uri objects instead of strings",
-            Justification = "URL isn't passed as variable, but as literal.")]
         public async Task HomeReturnsJson()
         {
             using var api = new LegacyApi();
